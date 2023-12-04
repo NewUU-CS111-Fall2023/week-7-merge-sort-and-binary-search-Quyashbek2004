bf57c361f86aafbd97eed89b8515a44c101fa043
Quyashbek2004 <127374399+Quyashbek2004@users.noreply.github.com>
2023-12-04 19:59:04 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,3 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Function to calculate remaining sum after operations
+void remainingSumAfterOperations(int T, vector<pair<int, vector<int>>>& testCases) {
+    for (int test = 0; test < T; ++test) {
+        int N = testCases[test].first;
+        vector<int>& arr = testCases[test].second;
+
+        vector<int> medians;
+        for (int i = 0; i < N - 2; ++i) { // O(N)
+            vector<int> subarray(arr.begin() + i, arr.begin() + i + 3); // O(1) - slicing
+            if (subarray.size() % 2 != 0) {
+                sort(subarray.begin(), subarray.end()); // O(1) - small size sorting
+                medians.push_back(subarray[1]); // O(1)
+            }
+        }
+
+        sort(medians.begin(), medians.end()); // O(KlogK) - K is the number of medians
+
+        int minMedian = (medians.size() > 0) ? medians[0] : *min_element(arr.begin(), arr.end()); // O(N) - finding min
+
+        arr.erase(find(arr.begin(), arr.end(), minMedian)); // O(N) - removing element
+
+        int remainingSum = 0;
+        for (int num : arr) { // O(N)
+            remainingSum += num;
+        }
+        cout << remainingSum << endl;
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    vector<pair<int, vector<int>>> testCases;
+    for (int i = 0; i < T; ++i) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int j = 0; j < N; ++j) {
+            cin >> arr[j];
+        }
+        testCases.push_back({ N, arr });
+    }
+
+    remainingSumAfterOperations(T, testCases);
+
+    return 0;
+}
+//Overall complexity: O(T * N^2 + T * NlogN)
 /*
  * Author:
  * Date:
